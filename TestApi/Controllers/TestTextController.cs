849774b467d75c866156a4bb0aae3ecdcf305243
Mattias Kågström <mattias.kagstrom95@@gmail.com>
2022-05-06 20:27:10 +0200

    Add project files.

---
+++ TestApi/Controllers/TestTextController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestTextController : ControllerBase
+    {
+        private readonly ILogger<TestTextController> _logger;
+
+        public TestTextController(ILogger<TestTextController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "Text")]
+        [Consumes("text/plain")]
+        public IEnumerable<string> StringConsumer([FromBody]string text)
+        {
+            return text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
